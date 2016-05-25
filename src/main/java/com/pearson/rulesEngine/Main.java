9d1cdd0db3a48fbad72e025a0ef96e65638a2d61
Udara Gunathilake <udara.gunathilake@pearson.com>
2016-05-25 17:37:11 +0530

    Changed annotations for best practices and to add components in Main method

--- src/main/java/com/pearson/rulesEngine/Main.java
+++ src/main/java/com/pearson/rulesEngine/Main.java
@@ -1,9 +1,14 @@
 package com.pearson.rulesEngine;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan({"com.pearson.rulesEngine.controller","com.pearson.rulesEngine.domain","com.pearson.rulesEngine.rules"})   // we can rplace all thses annotations with @SpringBootApplication.but it is good to add them all(can configure more easily)
+
 public class Main {
 
 	public static void main(String[] args) {

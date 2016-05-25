a4cf854a137904ee5cfcba4ad5b4d7bd82485d1c
Udara Gunathilake <udara.gunathilake@pearson.com>
2016-05-25 17:37:59 +0530

    Added Offer domain class

---
+++ src/main/java/com/pearson/rulesEngine/domain/Offer.java
@@ -0,0 +1,26 @@
+package com.pearson.rulesEngine.domain;
+
+public class Offer {
+String userRole;
+String countryCode;
+String orgId;
+public String getUserRole() {
+	return userRole;
+}
+public void setUserRole(String userRole) {
+	this.userRole = userRole;
+}
+public String getCountryCode() {
+	return countryCode;
+}
+public void setCountryCode(String countryCode) {
+	this.countryCode = countryCode;
+}
+public String getOrgId() {
+	return orgId;
+}
+public void setOrgId(String orgId) {
+	this.orgId = orgId;
+}
+
+}

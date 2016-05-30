50f6394f2cb03ffd57777873f423cc3b207bb44f
Irfan Fuard <UFUARIR@SRIL-CR5PYY1.PEROOT.com>
2016-05-30 11:37:47 +0530

    Get offer details from json file

--- src/main/java/com/pearson/rulesEngine/controller/OfferController.java
+++ src/main/java/com/pearson/rulesEngine/controller/OfferController.java
@@ -1,6 +1,12 @@
 package com.pearson.rulesEngine.controller;
 
+import java.io.FileReader;
+import java.io.IOException;
+
 import org.easyrules.api.RulesEngine;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,11 +21,16 @@ import com.pearson.rulesEngine.rules.OrgIdRule;
 @RestController
 public class OfferController {
 
+	@SuppressWarnings("resource")
 	@RequestMapping(value="/offer",method=RequestMethod.GET)
-	public Offer getOffer(@RequestParam(value="userRole")String userRole,@RequestParam(value="countryCode")String countryCode,@RequestParam(value="orgId")String orgId){
+	public Offer getOffer(@RequestParam(value="userRole")String userRole,@RequestParam(value="countryCode")String countryCode,@RequestParam(value="orgId")String orgId) throws IOException, ParseException{
 		
-		
+		final FileReader reader = new FileReader("src/main/resources/offers.json");
+        final JSONParser parser = new JSONParser();
+        final JSONObject json = (JSONObject) parser.parse(reader);
 	
+        System.out.println(json);
+        
 		ApplicationContext context = new ClassPathXmlApplicationContext("rules-engine-bean.xml");
 		RulesEngine rulesEngine = (RulesEngine) context.getBean("rulesEngine");
 		Offer offer =(Offer)context.getBean("Offer");

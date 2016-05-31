486b2dfba5c90d22662725e483d5db1c40c01965
Udara Gunathilake <udara.gunathilake@pearson.com>
2016-05-31 14:28:49 +0530

    Changed to validate OfferDto and added valid boolean parameter

--- src/main/java/com/pearson/rulesEngine/rules/OrgIdRule.java
+++ src/main/java/com/pearson/rulesEngine/rules/OrgIdRule.java
@@ -4,44 +4,77 @@ import org.easyrules.annotation.Action;
 import org.easyrules.annotation.Condition;
 import org.easyrules.annotation.Rule;
 
-import com.pearson.rulesEngine.domain.Offer;
+import com.pearson.rulesEngine.dto.OfferDto;
 
+/**Rule POJO which mapped with Rulebean in RulesBean
+ * If this rule passed valid will give True
+ * @author UGUNAUD
+ *
+ */
 @Rule(name = "OrgIdRule",
 description = "Validate organization rule")
 public class OrgIdRule {
+	
 	String[] orgIds;
-	Offer offer;
+	OfferDto offerDto;
+	boolean valid;
+	
+	/**
+	 * @return the valid
+	 */
+	public boolean isValid() {
+		return valid;
+	}
+
+	/**
+	 * @param valid the valid to set
+	 */
+	public void setValid(boolean valid) {
+		this.valid = valid;
+	}
+
+	/**
+	 * @return the orgIds
+	 */
 	public String[] getOrgIds() {
 		return orgIds;
 	}
+
+	/**
+	 * @param orgIds the orgIds to set
+	 */
 	public void setOrgIds(String[] orgIds) {
 		this.orgIds = orgIds;
 	}
 
-	
-	
-	
+	/**
+	 * @return the offerDto
+	 */
+	public OfferDto getOfferDto() {
+		return offerDto;
+	}
+
+	/**
+	 * @param offerDto the offerDto to set
+	 */
+	public void setOfferDto(OfferDto offerDto) {
+		this.offerDto = offerDto;
+	}
+
 	@Condition
 	public boolean checkOrgId(){
 		boolean equals=false;
 		for(int i=0;i<orgIds.length;i++){
-			if(orgIds[i].equals(offer.getOrgId())){
+			if(orgIds[i].equals(offerDto.getOrgId())){
 				equals=true;
 			}
 		}
 		return equals;
 	}
-	public Offer getOffer() {
-		return offer;
-	}
-
-	public void setOffer(Offer offer) {
-		this.offer = offer;
-	}
 
 	@Action
 	public void validOrgId(){
-		
+		setValid(true);
 		System.out.println("Offer is in valid Organization");
 	}
 }

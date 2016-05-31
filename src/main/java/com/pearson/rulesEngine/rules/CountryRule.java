8b24a930f1f8696069eec48d49ec1e99829c7382
Udara Gunathilake <udara.gunathilake@pearson.com>
2016-05-31 14:28:30 +0530

    Changed to validate OfferDto and added valid boolean parameter

--- src/main/java/com/pearson/rulesEngine/rules/CountryRule.java
+++ src/main/java/com/pearson/rulesEngine/rules/CountryRule.java
@@ -4,21 +4,60 @@ import org.easyrules.annotation.Action;
 import org.easyrules.annotation.Condition;
 import org.easyrules.annotation.Rule;
 
-import com.pearson.rulesEngine.domain.Offer;
+import com.pearson.rulesEngine.dto.OfferDto;
 
+/**Rule POJO which mapped with Rulebean in RulesBean
+ * If this rule passed valid will give True
+ * @author UGUNAUD
+ *
+ */
 @Rule(name = "CountryRule",
 description = "Validate country rule")
 public class CountryRule {
 
 	String[] countries;
-	Offer offer;
-	
+	OfferDto offerDto;
+	boolean valid;
+	/**
+	 * @return the valid
+	 */
+	public boolean isValid() {
+		return valid;
+	}
+
+	/**
+	 * @param valid the valid to set
+	 */
+	public void setValid(boolean valid) {
+		this.valid = valid;
+	}
+
+	/**
+	 * @return the countries
+	 */
 	public String[] getCountries() {
 		return countries;
 	}
 
+	/**
+	 * @param countries the countries to set
+	 */
 	public void setCountries(String[] countries) {
 		this.countries = countries;
+	}
+
+	/**
+	 * @return the offerDto
+	 */
+	public OfferDto getOfferDto() {
+		return offerDto;
+	}
+
+	/**
+	 * @param offerDto the offerDto to set
+	 */
+	public void setOfferDto(OfferDto offerDto) {
+		this.offerDto = offerDto;
 	}
 
 	public CountryRule(){
@@ -29,23 +68,16 @@ public class CountryRule {
 	public boolean checkCountry(){
 		boolean equals=false;
 		for(int i=0;i<countries.length;i++){
-			if(countries[i].equals(offer.getCountryCode())){
+			if(countries[i].equals(offerDto.getCountryCode())){
 				equals=true;
 			}
 		}
 		return equals;
 	}
-	public Offer getOffer() {
-		return offer;
-	}
-
-	public void setOffer(Offer offer) {
-		this.offer = offer;
-	}
 
 	@Action
 	public void validCountry(){
-		
+		setValid(true);
 		System.out.println("Offer is in valid country");
 	}
 }

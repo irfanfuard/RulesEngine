712d6c661f091ad2b3e39e0b570a9cf5c8b738f0
Udara Gunathilake <udara.gunathilake@pearson.com>
2016-06-02 11:07:49 +0530

    Extended from BaseRule .Overide Action and Condition methods(Added easyrules annotations).

--- src/main/java/com/pearson/rulesEngine/rules/CountryRule.java
+++ src/main/java/com/pearson/rulesEngine/rules/CountryRule.java
@@ -4,8 +4,6 @@ import org.easyrules.annotation.Action;
 import org.easyrules.annotation.Condition;
 import org.easyrules.annotation.Rule;
 
-import com.pearson.rulesEngine.dto.OfferDto;
-
 /**
  * A POJO which is mapped with country rule in the bean xml
  * @author UGUNAUD
@@ -13,65 +11,19 @@ import com.pearson.rulesEngine.dto.OfferDto;
  */
 @Rule(name = "CountryRule",
 description = "Validate country rule")
-public class CountryRule {
+public class CountryRule extends BaseRule<String>{
 
-	String[] countries;
-	OfferDto offerDto;
-	boolean valid;
-	/**
-	 * @return the valid
+	/* (non-Javadoc)
+	 * @see com.pearson.rulesEngine.rules.BaseRule#condition()
 	 */
-	public boolean isValid() {
-		return valid;
-	}
-
-	/**
-	 * @param valid the valid to set
-	 */
-	public void setValid(boolean valid) {
-		this.valid = valid;
-	}
-
-	/**
-	 * @return the countries
-	 */
-	public String[] getCountries() {
-		return countries;
-	}
-
-	/**
-	 * @param countries the countries to set
-	 */
-	public void setCountries(String[] countries) {
-		this.countries = countries;
-	}
-
-	/**
-	 * @return the offerDto
-	 */
-	public OfferDto getOfferDto() {
-		return offerDto;
-	}
-
-	/**
-	 * @param offerDto the offerDto to set
-	 */
-	public void setOfferDto(OfferDto offerDto) {
-		this.offerDto = offerDto;
-	}
-
-	public CountryRule(){
-		
-	}
-	
-	//Check if the country rule is valid or not
+	@Override
 	@Condition
-	public boolean checkCountry(){
+	public boolean condition() {
 		boolean equals=false;
 		
 		//If the country code of the specific offer matches any of the codes mentioned in the bean xml return true
-		for(int i=0;i<countries.length;i++){
-			if(countries[i].equals(offerDto.getCountryCode())){
+		for(int i=0;i<beanValues.length;i++){
+			if(beanValues[i].equals(offer.getCountryCode())){
 				equals=true;
 			}else{
 				setValid(false);
@@ -79,11 +31,16 @@ public class CountryRule {
 		}
 		return equals;
 	}
-	
-	//Perform action
+
+	/* (non-Javadoc)
+	 * @see com.pearson.rulesEngine.rules.BaseRule#action()
+	 */
+	@Override
 	@Action
-	public void validCountry(){
-		setValid(true);
-		System.out.println("Country code is valid for the specific offer");
+	public void action() {
+		System.out.println("Country Rule validated");
+		super.action();
 	}
+
+
 }
